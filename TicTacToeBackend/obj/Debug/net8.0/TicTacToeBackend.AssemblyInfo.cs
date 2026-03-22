a14934ed1d7f2468a05ccade8e1e86f47cd6f82c
Jatinderpal Singh <jatinderpal.singh@zynga.com>
2026-03-22 10:57:46 +0530

    Docker

--- TicTacToeBackend/obj/Debug/net8.0/TicTacToeBackend.AssemblyInfo.cs
+++ TicTacToeBackend/obj/Debug/net8.0/TicTacToeBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39119b09afd64f6ff7034f0711a1f9e86642a83e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9b3e0ea6ccb20cfa26458ac06cb926092dcb27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
